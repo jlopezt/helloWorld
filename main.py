fea74f88b2cffe0faa76345b20e15dccc3294163
JoLoTo <jose.lopeztola@gmail.com>
2025-05-01 00:45:52 +0200

    Incluyo puerto e IP donde levantar

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,4 +7,5 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
-app.run()
+port = int(os.environ.get("PORT", 10000))  # Render usa esta variable
+app.run(host="0.0.0.0", port=port)
